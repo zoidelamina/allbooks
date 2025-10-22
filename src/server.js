83a7cc0a8c6cd4570dc228ada4d35871204f72cc
Otavio Santos <otaviomcsa@gmail.com>
2025-10-22 11:24:36 -0300

    Altera o servidor.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Boas-vindas a API do AllBooks")
+  console.log("API disponível através da url http://localhost:8000")
 })
